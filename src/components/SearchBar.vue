836499d67c17050fb429a8f65e915d5b6d9ce74f
jorsh <jorshyy19367@gmail.com>
2026-03-04 22:29:05 -0700

    add search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="search-bar">
+        <input type="text" placeholder="Search..." />
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Search Bar"
+    }
+  }
+}
+</script>
